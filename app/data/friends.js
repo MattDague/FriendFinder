2cc8d067a0659e3127535e5fd1f5845127016d45
MattDague <mattdague@gmail.com>
2019-08-04 20:32:35 -0400

    forumla for calculating best match done

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,37 +1,117 @@
 
 var Friends = [
     {
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+        "name": "Laura",
+        "photo": "https://vignette.wikia.nocookie.net/twinpeaks/images/9/92/Laura_Palmer_%281989%29.jpg/revision/latest?cb=20180219220542",
         "scores": [
-        "5",
-        "1",
-        "4",
-        "4",
-        "5",
-        "1",
-        "2",
-        "5",
-        "4",
-        "1"
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
         ]
-        },
-        {
-        "name": "Jacob Deming",
+    },
+    {
+        "name": "Mrs. Featherbottom",
+        "photo": "https://vignette.wikia.nocookie.net/arresteddevelopment/images/4/47/2x14_The_Immaculate_Election_%2872%29.png/revision/latest?cb=20130129170207",
+        "scores": [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+    },
+    {
+        "name": "Patrick Star",
+        "photo": "https://vignette.wikia.nocookie.net/nickelodeon-movies/images/7/7e/Patrick_Star.png/revision/latest?cb=20180107193038",
+        "scores": [
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1"
+        ]
+    },
+    {
+        "name": "George",
         "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
         "scores": [
-        "4",
-        "2",
-        "5",
-        "1",
-        "3",
-        "2",
-        "2",
-        "1",
-        "3",
-        "2"
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2"
         ]
-        },
-    
+    },
+    {
+        "name": "Frank",
+        "photo": "https://scontent-lga3-1.cdninstagram.com/vp/18e13bdc408c9e786e847002cbb16309/5DECE598/t51.2885-15/e35/15877269_615317958672461_3534780843639701504_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com",
+        "scores": [
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3"
+        ]
+    },
+    {
+        "name": "Lucille",
+        "photo": "https://www.pedestrian.tv/content/uploads/2016/09/lucille-619-386.jpg",
+        "scores": [
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4"
+        ]
+    },
+    {
+        "name": "DumbleDore",
+        "photo": "https://vignette.wikia.nocookie.net/harrypotter/images/2/20/Albus_Dumbledore.JPG/revision/latest?cb=20080706131332",
+        "scores": [
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5"
+        ]
+    },
+
 ]
- module.exports = Friends
+module.exports = Friends
